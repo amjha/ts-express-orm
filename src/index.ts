d924b314f338d3d0a2942376f38bc4a70a17e583
amjha <amitjha@usc.edu>
2020-06-05 16:02:15 -0400

    CLI added

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,19 @@
+#!/usr/bin/env node
+const chalk = require('chalk');
+const figlet = require('figlet');
+const program = require('commander');
 import * as prompt from "./prompt";
+
+console.log(
+    chalk.red(
+      figlet.textSync('TSEO', { horizontalLayout: 'full' })
+    )
+  );
+
+  program
+  .version('0.0.4')
+  .description("A minimalistic NodeJS based Typescript, Expressjs and TypeORM boilerplate generator")
+  .option('-O, --no-typeorm', 'You do not want any TypeORM')
+  .parse(process.argv);
+
 prompt.promptFn();
